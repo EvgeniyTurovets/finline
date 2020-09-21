0e8b1575944d28dc8a9b482c93aed3ecc998fead
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-09-21 12:53:19 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -2,4 +2,25 @@ $(function(){
 	$('input[type="phone"]').mask("+7(999) 999-9999"); 
 
 	$('select').selectric(); 
+
+	
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("outpup");
+	output.innerHTML = slider.value; // Display the default slider value
+
+	// Update the current slider value (each time you drag the slider handle)
+	slider.oninput = function() {
+	output.innerHTML = this.value;
+	}
+
+	$('#accordeon .acc-head').on('click', f_acc);
+
+	function f_acc(){
+	$('.acc-head').not($(this)).removeClass('active')
+	$(this).toggleClass('active')
+	//скрываем все кроме того, что должны открыть
+	  $('#accordeon .acc-body').not($(this).next()).slideUp(500);
+	// открываем или скрываем блок под заголовком, по которому кликнули
+	    $(this).next().slideToggle(500);
+	}
 })
